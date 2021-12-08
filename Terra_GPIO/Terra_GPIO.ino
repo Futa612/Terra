85d1b2ae0827e63a4bd7aefbe27cc3b8de50957f
Dat Nguyen <90817736+Futa612@users.noreply.github.com>
2021-12-08 16:38:09 +0700

    main_GPIO

---
+++ Terra_GPIO/Terra_GPIO.ino
@@ -0,0 +1,106 @@
+//Tham khao tu Rui
+
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+#include <WiFiClient.h>
+#include <Arduino_JSON.h>
+#include <DNSServer.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266WiFiMulti.h>
+#include <DHT.h>
+
+#define DHTPIN 14 //chon pin 
+#define DHTTYPE DHT11 //dht11
+
+DHT dht(DHTPIN, DHTTYPE);
+
+ESP8266WiFiMulti WiFiMulti;
+
+#include <WiFiManager.h>         // https://github.com/tzapu/WiFiManager
+
+//const char* host = "ezroom.000webhostapp.com"; //host thi bo http
+const char* host = "xemsao.com"; //host thi bo http
+
+// Your IP address or domain name with URL path
+const char* serverName = "http://xemsao.com/esp-outputs-action.php?action=outputs_state&board=1";
+
+// Update interval time set to 1 seconds
+const long interval = 5000;
+unsigned long previousMillis = 0;
+
+String outputsState;
+
+void setup() {
+  Serial.begin(115200);
+  WiFiManager wifiManager;
+  wifiManager.autoConnect("Terra");
+  Serial.println("Connected.");
+}
+
+void loop() {
+  unsigned long currentMillis = millis();
+  
+  if(currentMillis - previousMillis >= interval) {
+     // Check WiFi connection status
+    if ((WiFiMulti.run() == WL_CONNECTED)) {
+      outputsState = httpGETRequest(serverName);
+//      Serial.println(outputsState);
+      JSONVar myObject = JSON.parse(outputsState);
+  
+      // JSON.typeof(jsonVar) can be used to get the type of the var
+      if (JSON.typeof(myObject) == "undefined") {
+        Serial.println("Parsing input failed!");
+        return;
+      }
+    
+      Serial.print("JSON object = ");
+      Serial.println(myObject);
+    
+      // myObject.keys() can be used to get an array of all the keys in the object
+      JSONVar keys = myObject.keys();
+    
+      for (int i = 0; i < keys.length(); i++) {
+        JSONVar value = myObject[keys[i]];
+//        Serial.print("GPIO: ");
+//        Serial.print(keys[i]);
+//        Serial.print(" - SET to: ");
+//        Serial.println(value);
+        pinMode(atoi(keys[i]), OUTPUT);
+        digitalWrite(atoi(keys[i]), atoi(value));
+      }
+      // save the last HTTP GET Request
+      previousMillis = currentMillis;
+    }
+    else {
+      Serial.println("WiFi Disconnected");
+    }
+  }
+
+}
+
+String httpGETRequest(const char* serverName) {
+  WiFiClient client;
+  HTTPClient http;
+    
+  // Your IP address with path or Domain name with URL path 
+  http.begin(client, serverName);
+  
+  // Send HTTP POST request
+  int httpResponseCode = http.GET();
+  
+  String payload = "{}"; 
+  
+  if (httpResponseCode>0) {
+//    Serial.print("HTTP Response code: ");
+//    Serial.println(httpResponseCode);
+    payload = http.getString();
+  }
+  else {
+    Serial.print("Error code: ");
+    Serial.println(httpResponseCode);
+  }
+  // Free resources
+  http.end();
+
+  return payload;
+}
