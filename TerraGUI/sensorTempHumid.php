0f912ec340af62ef460993b62c08ea27859032e0
Dat Nguyen <90817736+Futa612@users.noreply.github.com>
2022-03-24 13:57:19 +0700

    Update and rename sensor.php to sensorTempHumid.php

--- TerraGUI/sensorTempHumid.php
+++ TerraGUI/sensorTempHumid.php
@@ -9,17 +9,13 @@
     
     $temp = $_GET["temp"];
     $humid = $_GET["humid"];
-    $lux = $_GET["lux"];
 
-    // $temp = "20";
-    // $humid = "43";
-    // $lux = "54";
-
-    $sql1 = "UPDATE `sensor` SET `temp`='".$temp."',`humid`='".$humid."',`lux`='".$lux."' WHERE 1";
+    //Update 2 parameters 
+    $sql1 = "UPDATE `sensor` SET `temp`='".$temp."',`humid`='".$humid."' WHERE 1";
 
     $result1 = mysqli_query($connect, $sql1);
     if ($result != NULL) {
-        echo "Da nhan du lieu tu ESP8266<br>";
+        echo "Data from ESP8266 was received<br>";
     }
     mysqli_close($connect);
 ?>
