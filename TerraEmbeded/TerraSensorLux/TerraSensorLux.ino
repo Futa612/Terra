ebc1ebcdf7b0f0a4078ddbdf584fa8c0b5c4ea14
hoan2510 <hoanvan0603@gmail.com>
2022-03-24 21:04:28 +0700

    sua file terrasensorlux

--- TerraEmbeded/TerraSensorLux/TerraSensorLux.ino
+++ TerraEmbeded/TerraSensorLux/TerraSensorLux.ino
@@ -6,10 +6,12 @@
 #include <ESP8266WebServer.h>
 #include <ESP8266WiFiMulti.h>
 #include <DHT.h>
+#include <Wire.h>
+#include <BH1750.h>
 
 #define DHTPIN 5 //chon pin 
 #define DHTTYPE DHT11 //dht11
-
+BH1750 lightMeter;
 DHT dht(DHTPIN, DHTTYPE);
 
 ESP8266WiFiMulti WiFiMulti;
@@ -29,10 +31,12 @@ void sendRequestLux(float lux);
 float getLux();
 
 void setup() {
+   Wire.begin();
   Serial.begin(115200);
   WiFiManager wifiManager;
   wifiManager.autoConnect("Terra");
   Serial.println("Connected.");
+  lightMeter.begin();
 }
 
 void loop() {
@@ -43,8 +47,8 @@ void loop() {
 }
 
 float getLux() {
-  //Code here...
-  float lux = random(20, 35);
+  float lux = lightMeter.readLightLevel();
+  Serial.print("Light: ");
   Serial.println(lux);
   return lux;
 }
