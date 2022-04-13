8f50e0d5de9025cf2de7de34ac5f4a49807bd724
hoan2510 <hoanvan0603@gmail.com>
2022-04-13 10:12:11 +0700

    add source code & gui du lieu do am dat

--- TerraEmbeded/TerraPID/TerraPID.ino
+++ TerraEmbeded/TerraPID/TerraPID.ino
@@ -1,5 +1,17 @@
 #include <PID_v1.h>
+#include <Wire.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+#include <WiFiClient.h>
+#include <Arduino_JSON.h>
+#include <DNSServer.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266WiFiMulti.h>
+#include <WiFiManager.h>
+ESP8266WiFiMulti WiFiMulti;
+const char* host = "xemsao.com";
 float adc;
+float soil;
 double Setpoint, Input, Output;
 double Kp = 5, Ki = 1.5, Kd = 0.35;
 PID myPID(&Input, &Output, &Setpoint, Kp, Ki, Kd, DIRECT);
@@ -15,13 +27,53 @@ void setup()
 }
 void loop()
 {
-  doam();
+  myPid();
+  unsigned long currentMillis = millis();
+  soil = getSoil();
+  sendRequestLux(soil);
+}
+float getSoil() {
+  adc = analogRead(A0);
+  float soil = map(adc, 0, 1023, 100, 0);
+  Serial.print("Soil: ");
+  Serial.println(soil);
+  return soil;
+}
+void myPid(void)
+{
   adc = analogRead(A0);
   Input = map(adc, 0, 1023, 100, 0);
   myPID.Compute();
   analogWrite(D1, Output);
 }
-void doam (void) {
-  Serial.println(Input);
-  delay(1000);
+void sendRequestLux(float soil) {
+  //1. TCP connection
+  WiFiClient client;
+  HTTPClient http;
+  const int httpPort = 80;
+
+  //Check connection
+  Serial.print("Connecting to: ");
+  Serial.println(host);
+  if (!client.connect(host, httpPort)) {
+    Serial.println("Connection fail!");
+    return;
+  }
+
+  //Send request to the sever
+  client.print(String("GET https://xemsao.com/sensorLux.php?")
+               + ("&lux=") + soil
+               + " HTTP/1.1\r\n"
+               + "Host: " + host + "\r\n"
+               + "Connection: close\r\n\r\n");
+
+  unsigned long current_time = millis();
+
+  while (client.available() == 0) {
+    if (millis() - current_time > 6000) {
+      Serial.print(">>> Client stop");
+      client.stop();
+      return;
+    }
+  }
 }
